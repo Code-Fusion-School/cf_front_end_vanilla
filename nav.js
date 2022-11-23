7beaeacbe8f74896a6e436858d62dd0ee3d2a29e
lg550055 <lg550055@hotmail.com>
2022-11-23 11:05:36 -0600

    Adds link to advanced page

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ document.write(`
     <div><a href="./index.html"><img src="img/cflogo.png" alt="logo" /></a></div>
     <a href="./courses.html">Courses</a>
     <a href="./faq.html">FAQ</a>
-    <a href="./finance.html">Finance</a>
+    <a href="./advanced.html">Advanced</a>
     <button onclick="(()=>applymodal.style.display='block')()">Get started</button>
   </nav>
 `);
